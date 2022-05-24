0a0bc31afaa44dde2e6479ef5f28598ac0c4e327
HumXC <Hum-XC@outlook.com>
2022-05-24 11:05:50 +0800

    完成部分virturldesktop功能

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ namespace AutoVDesktop
     using System;
     using AutoVDesktop.IconsRestorer;
     using System.Diagnostics;
+    using Microsoft.Win32;
+    using System.Management;
+    using AutoVDesktop.VirtualDesktop;
+    using System.Security.Principal;
+    using System.Text;
 
     internal static class Program
     {
@@ -11,11 +16,23 @@ namespace AutoVDesktop
         public static Config config = new();
         private static readonly object lockObj = new();
         private static int threadID = 0;
+        static private void registryEventHandler(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Received an event:");
 
+            //Iterate over the properties received from the event and print them out.
+            foreach (var prop in e.NewEvent.Properties)
+            {
+                Logger.Debug($"{prop.Name}:{prop.Value}");
+            }
+
+        }
 
         [STAThread]
         static void Main()
         {
+           
+       
             Process[] processes = System.Diagnostics.Process.GetProcessesByName(Application.CompanyName);
             if (processes.Length > 1)
             {
@@ -23,7 +40,7 @@ namespace AutoVDesktop
                 Environment.Exit(0);
             }
 
-           
+
             InitConf(config);
             /*            VirtualDesktop.CurrentChanged += (_, args) =>
                         {
@@ -31,6 +48,7 @@ namespace AutoVDesktop
                             ThreadPool.QueueUserWorkItem((state) => { ChangeDesktop(args.NewDesktop.Name, threadID); });
                             ++threadID;
                         };*/
+            Logger.Debug(VirtualDesktop.VirtualDesktop.NowDesktop.Name);
             Application.Run(new OptionView());
 
         }
@@ -89,7 +107,7 @@ namespace AutoVDesktop
         }
         static void SaveIcon(string desktopName)
         {
-            var desktop = new IconsRestorer. Desktop();
+            var desktop = new IconsRestorer.Desktop();
             IconsRestorer.Desktop.Refresh();
             var iconPositions = desktop.GetIconsPositions();
             // var registryValues = _registry.GetRegistryValues();
@@ -118,31 +136,42 @@ namespace AutoVDesktop
                 Logger.Debug("这里是Debug窗口,可以在配置文件里将[DebugMode]属性改为false关闭该窗口的显示.");
             }
             //开机自启
-            Microsoft.Win32.RegistryKey RKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
-            var appName = Environment.ProcessPath;
-            if (appName != null)
+            var RKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+            if (RKey == null)
             {
-                var k = RKey.GetValue("AutoVDesktop");
-                if (k == null)
+                MessageBox.Show("错误", "设置自启动失败, 无法打开注册表。", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var appName = Environment.ProcessPath;
+            if (appName == null)
+            {
+                MessageBox.Show("错误", "设置自启动失败, 程序名为 null 。", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var value = RKey.GetValue("AutoVDesktop");
+            if (value == null)
+            {
+                if (config.StartWithWindows)
+                    RKey.SetValue("AutoVDesktop", appName);
+            }
+            else
+            {
+                if (config.StartWithWindows)
                 {
-                    if (config.StartWithWindows)
+                    if (!value.Equals(appName))
+                    {
                         RKey.SetValue("AutoVDesktop", appName);
+                    }
                 }
                 else
                 {
-                    if (config.StartWithWindows)
-                    {
-                        if (!k.Equals(appName))
-                        {
-                            RKey.SetValue("AutoVDesktop", appName);
-                        }
-                    }
-                    else
-                    {
-                        RKey.DeleteValue("AutoVDesktop");
-                    }
+                    RKey.DeleteValue("AutoVDesktop");
                 }
             }
+
+
+
         }
         public static class Logger
         {
--- VirtualDesktop/VirtualDesktop.cs
+++ VirtualDesktop/VirtualDesktop.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +12,129 @@ namespace AutoVDesktop.VirtualDesktop
 {
     internal class VirtualDesktop
     {
+        private static WqlEventQuery changedQuery;
+        private static WqlEventQuery creatOrRmQuery;
 
-    }
-    public class Desktop
-    {
-        public string Name { get; }
-        public Guid Guid { get; }
-        public Desktop(string name, string guid)
+        public static Desktop NowDesktop { get; private set; }
+        public static List<Desktop> Desktops { get; private set; } = new List<Desktop>();
+        static VirtualDesktop()
         {
-            Name = name;
-            Guid = new(guid);
+            // 注册 注册表监听器
+            var currentUser = WindowsIdentity.GetCurrent();
+            if (currentUser.User == null)
+            {
+                throw new Exception("无法获取用户信息");
+            }
+            VirtualDesktop.changedQuery = new WqlEventQuery(string.Format(
+                "SELECT * FROM RegistryValueChangeEvent WHERE Hive='HKEY_USERS' AND KeyPath='{0}\\\\{1}' AND ValueName='{2}'",
+                currentUser.User.Value, @"Software\Microsoft\Windows\CurrentVersion\Explorer\VirtualDesktops".Replace("\\", "\\\\"), "CurrentVirtualDesktop"));
+            VirtualDesktop.creatOrRmQuery = new WqlEventQuery(string.Format(
+                "SELECT * FROM RegistryTreeChangeEvent WHERE Hive='HKEY_USERS' AND RootPath='{0}\\\\{1}'",
+                 currentUser.User.Value, @"Software\Microsoft\Windows\CurrentVersion\Explorer\VirtualDesktops\Desktops".Replace("\\", "\\\\")));
+
+            var _watcher = new ManagementEventWatcher(VirtualDesktop.changedQuery);
+            _watcher.EventArrived += (sender, args) =>
+            {
+                Program.Logger.Debug("切换桌面");
+            };
+            _watcher.Start();
+            var _watcher2 = new ManagementEventWatcher(VirtualDesktop.creatOrRmQuery);
+            _watcher2.EventArrived += (sender, args) =>
+            {
+                Program.Logger.Debug("创建桌面");
+            };
+            _watcher2.Start();
+            var guidList = GetDeskGuid();
+            foreach (var guid in guidList)
+            {
+                var desk = new Desktop(guid);
+                VirtualDesktop.Desktops.Add(desk);
+            }
+
+            NowDesktop = GetNowDesktop();
+        }
+        static private List<Guid> GetDeskGuid()
+        {
+            List<Guid> list = new();
+            var reg = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\VirtualDesktops\Desktops");
+            if (reg == null)
+            {
+                return list;
+            }
+            foreach (string guid in reg.GetSubKeyNames())
+            {
+                list.Add(Guid.Parse(guid));
+            }
+            return list;
+        }
+
+        private static Desktop GetNowDesktop()
+        {
+            var reg = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\VirtualDesktops");
+            if (reg == null)
+            {
+                throw new Exception("无法获取当前桌面, 注册表项为空。");
+            }
+            if (reg.GetValue("CurrentVirtualDesktop") is not byte[] b)
+            {
+                throw new Exception("当前桌面的值为空");
+            }
+            return new Desktop(ByteToGuid(b));
+        }
+        public static Guid ByteToGuid(byte[] bytes)
+        {
+            string[] strs = new string[8] { string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, };
+            strs[1] = Convert.ToString(BitConverter.ToInt16(new byte[2] { bytes[0], bytes[1] }, 0), 16).PadLeft(4, '0');
+            strs[0] = Convert.ToString(BitConverter.ToInt16(new byte[2] { bytes[2], bytes[3] }, 0), 16).PadLeft(4, '0');
+            strs[2] = Convert.ToString(BitConverter.ToInt16(new byte[2] { bytes[4], bytes[5] }, 0), 16).PadLeft(4, '0');
+            strs[3] = Convert.ToString(BitConverter.ToInt16(new byte[2] { bytes[6], bytes[7] }, 0), 16).PadLeft(4, '0');
+            strs[4] = Convert.ToString(BitConverter.ToInt16(new byte[2] { bytes[9], bytes[8] }, 0), 16).PadLeft(4, '0');
+            strs[5] = Convert.ToString(bytes[10], 16).PadLeft(2, '0') + Convert.ToString(bytes[11], 16).PadLeft(2, '0');
+            strs[6] = Convert.ToString(bytes[12], 16).PadLeft(2, '0') + Convert.ToString(bytes[13], 16).PadLeft(2, '0');
+            strs[7] = Convert.ToString(bytes[14], 16).PadLeft(2, '0') + Convert.ToString(bytes[15], 16).PadLeft(2, '0');
+            return new Guid(String.Format("{0}{1}-{2}-{3}-{4}-{5}{6}{7}", strs[0], strs[1], strs[2], strs[3], strs[4], strs[5], strs[6], strs[7]));
+        }
+        class CurrentChangedObj
+        {
+            public Desktop OldDesktop { get; private set; }
+            public Desktop NewDesktop { get; private set; }
+            public CurrentChangedObj(Desktop oldDesk, Desktop newDesk)
+            {
+                OldDesktop = oldDesk;
+                NewDesktop = newDesk;
+            }
         }
     }
+    class Desktop
+    {
+        public string Name { get; private set; }
+        public string Wallpaper { get; private set; }
+        public Guid Guid { get; private set; }
+        private readonly RegistryKey reg;
+        public Desktop(Guid guid)
+        {
+
+            Guid = guid;
+
+            var r = Registry.CurrentUser.OpenSubKey($@"Software\Microsoft\Windows\CurrentVersion\Explorer\VirtualDesktops\Desktops\{{{guid}}}",true);
+            if (r == null)
+            {
+                throw new Exception("无法打开桌面的注册表项: Guid=" + guid);
+            }
+            reg = r;
+            var _name = r.GetValue("Name");
+            if (_name == null)
+            {
+                throw new Exception("桌面名称为空");
+            }
+            var _wallpaper = r.GetValue("Wallpaper");
+            if (_wallpaper == null)
+            {
+                throw new Exception("桌面壁纸为空");
+            }
+            Name = (string)_name;
+            Wallpaper = (string)_wallpaper;
+        }
+    }
+
 }
